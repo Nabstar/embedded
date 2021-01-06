629b152540c20669ad7a6e1bdd16da076d9c948e
Lukman <lukmaan.me@gmail.com>
2021-01-06 22:20:04 +0530

    css loader added

--- _site/assets/js/script.js
+++ _site/assets/js/script.js
@@ -1,5 +1,29 @@
-commentform.onsubmit = async(e) => {
+(function ($) {
 
-    console.log("form submitted");
+    console.log("hsd");
 
-}
+    $("form").on("submit", function(event) {
+
+        event.preventDefault();
+
+        $(".submitbtn").prop("disabled", true);
+        $(".lds-ring").toggle();
+
+        var formValues=$(this).serialize();
+
+        $.post('https://api-bridge-staticman.herokuapp.com/v2/entry/Nabstar/blog/master/comments', formValues, function(data) {
+
+            resetForm();
+            $(".submitbtn").prop("disabled", false);
+            $(".lds-ring").toggle();
+
+        });
+
+    });
+
+    function resetForm() {
+        $(".visitorname input").val("");
+        $(".visitorcomment textarea").val("");
+    }
+
+})(jQuery);
